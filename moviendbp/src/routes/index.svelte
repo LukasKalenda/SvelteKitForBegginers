7b5bc5fa31a58ab381869752f53cedc7e8f19134
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2022-07-01 14:51:33 +0200

    pred smazanim obsahu index.html

--- moviendbp/src/routes/index.svelte
+++ moviendbp/src/routes/index.svelte
@@ -1,7 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+
+    let count = 0;
+
+    function pricitani(){
+        count +=1;
+    }
+</script>
+
+
+<h1>Number: {count}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
     
     <button>Já jsem tlacitko</button>
+    <p>Zde si klikni na about page <a href="/about">About</a></p>
+
+    <button on:click={pricitani}>Pricitani</button>
+
+
+
+
+
 
     <style>
         p{
