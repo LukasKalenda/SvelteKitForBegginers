02ac09ee8cff8c14fc1770861af9300f67f4bbec
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2022-07-01 21:40:39 +0200

    pred yablko tutoritalem

---
+++ moviendbp/src/components/PopularMovies.svelte
@@ -0,0 +1,14 @@
+<script>
+export let popular;
+console.log(popular);
+</script>
+
+<h1>Popular Movies</h1>
+<div class="popular-movies">
+	<div class="movie"/>
+
+    {#each popular as movie}
+    <h1>{movie.title}</h1>
+    {/each}
+
+</div>
--- moviendbp/src/routes/index.svelte
+++ moviendbp/src/routes/index.svelte
@@ -1,11 +1,26 @@
-<script contextmenu="module">
-	const apiKey = dda0d99160a176b43d0b9af81da127fc;
+<script context="module">
+	
 
 	export async function load({ fetch }) {
 		const res = await fetch(
-			`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+			`https://api.themoviedb.org/3/movie/popular?api_key=dda0d99160a176b43d0b9af81da127fc&language=en-US&page=1`
 		);
 
-        const data = await res.json();
+		const data = await res.json();
+        console.log(data);
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
 	}
 </script>
+
+<script>
+    import PopularMovies from "../components/PopularMovies.svelte";
+    export let popular;
+</script>
+
+<section>
+    <PopularMovies {popular}/>
+</section>
